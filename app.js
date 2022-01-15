98500b612d718bc6c46518bee8723e795e56deef
LeoMosiah <leonardomosiah@gmail.com>
2022-01-15 16:53:22 +0100

    lesson 4

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const path = require('path');
-
+const dotenv = require('dotenv');
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const corsOptions = {
+  origin: "https://leo-node-js-complete-guide.herokuapp.com/",
+  optionsSuccessStatus: 200
+}
+
+const mongoOptions = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+}
 
 const app = express();
+dotenv.config()
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -13,12 +30,44 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('61e2e1c0ace7950f48bf7dda')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+mongoose
+  .connect(
+    process.env.MONGO_URL,
+    mongoOptions
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
