55468d42a0ff9d984717e1bfadbe4a25ad1aba14
LeoMosiah <leonardomosiah@gmail.com>
2022-01-23 00:36:59 +0100

    lesson 6

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 dotenv.config()
 
@@ -30,6 +32,7 @@ const store = new MongoDBStore({
   uri: process.env.MONGO_URL,
   collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 
 app.set('view engine', 'ejs');
@@ -50,6 +53,14 @@ app.use(
     store: store
   })
 );
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -57,17 +68,32 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then(user => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      next(new Error(err));
+    });
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 mongoose
   .connect(
