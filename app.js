24f99b3a7de4736ec5b3cbbcaf1d9af28e8e493d
LeoMosiah <leonardomosiah@gmail.com>
2022-01-26 23:32:47 +0100

    lesson 7

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer');
 
 dotenv.config()
 
@@ -34,6 +35,27 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -44,6 +66,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
